9b3542419ec2b7b3fa3d026d95dedf1f4043bbfa
Igor Todorovski <itodorov@ca.ibm.com>
2020-12-29 14:51:38 -0500

    Add read/copy/data and modify obj object name to response

--- examples/datasets/index.js
+++ examples/datasets/index.js
@@ -8,3 +8,7 @@ zoau.datasets.listing("SYS1.PARM*", 1).then(console.log)
 zoau.datasets.exists("SYS1.PARMLIB").then(console.log)
 
 zoau.datasets.list_members("SYS1.PARMLIB").then(console.log).catch(console.error);
+
+zoau.datasets.read("'SYS1.PARMLIB.RACF(IRROPT99)'").then(console.log).catch(console.error);
+
+zoau.datasets.copy("/etc/profile", "'ITODORO.MYPROF2'").then(console.log).catch(console.error);
