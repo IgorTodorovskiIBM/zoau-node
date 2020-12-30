dba0152d1245368784e66cbec47eebc286d3998d
Igor Todorovski <itodorov@ca.ibm.com>
2020-12-30 12:20:13 -0500

    Add create function and fix dataset parse

--- examples/datasets/index.js
+++ examples/datasets/index.js
@@ -1,5 +1,7 @@
 var zoau = require('../../')
 
+const ID = process.env.USER
+
 zoau.datasets.listing("SYS1.PARM*", 1).then(console.log)
 .catch(function (err) {
      console.log(err);
@@ -11,4 +13,7 @@ zoau.datasets.list_members("SYS1.PARMLIB").then(console.log).catch(console.error
 
 zoau.datasets.read("'SYS1.PARMLIB.RACF(IRROPT99)'").then(console.log).catch(console.error);
 
-zoau.datasets.copy("/etc/profile", "'ITODORO.MYPROF2'").then(console.log).catch(console.error);
+var details = { "primary_space" : 10 }
+zoau.datasets.create(`'${ID}.MYPROF5'`, details).then(console.log).catch(console.error);
+
+zoau.datasets.copy("/etc/profile", `'${ID}.MYPROF2'`).then(console.log).catch(console.error);
