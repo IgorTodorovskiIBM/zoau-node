965cddfd664d326956c0b94c9660d87e34258885
Gaby Baghdadi <baghdadi@ca.ibm.com>
2021-01-07 10:51:58 -0500

    test: tmp_name function

--- examples/datasets/hlq-tmp_name.js
+++ examples/datasets/hlq-tmp_name.js
@@ -9,17 +9,22 @@ function errfunc(err) {
 
 async function test() {
  try {
+  //---------------hlq
   console.log("Test: hlq...");
   var res = await zoau.datasets.hlq().catch(console.error);
   if (res !== ID) {
     console.error(`Error: hlq returned ${res}, expected ${ID}`);
     process.exitCode = -1; return;
   }
+
+  console.log("Test: hlq -d...");
   res = await zoau.datasets.hlq({"debug":true}).catch(console.error);
   if (res !== ID) {
     console.error(`Error: hlq -d returned ${res}, expected ${ID}`);
     process.exitCode = -1; return;
   }
+
+  console.log("Test: _hlq -d...");
   res = await zoau.datasets._hlq({"debug":true}).catch(console.error);
   console.log('exit=<' + res["exit"] + '>');
   console.log('stdout=<' + res["stdout"] + '>');
@@ -28,6 +33,35 @@ async function test() {
     process.exitCode = -1; return;
   }
   console.log('stderr=<' + res["stderr"] + '>');
+
+  //---------------tmp_name
+  console.log("Test: tmp_name...");
+  var res = await zoau.datasets.tmp_name().catch(console.error);
+  console.log(`res=${res}`);
+  var dots = (res.match(/\./g) || []).length
+  if (!res.startsWith("MVSTMP.") || res.length < 33 || dots < 3) {
+    console.error(`Error: string returned by tmp_name expected to start with 'MVSTMP.', got ${res}`);
+    process.exitCode = -1; return;
+  }
+
+  console.log("Test: tmp_name with HLQ of the temp dataset name...");
+  res = await zoau.datasets.tmp_name("ABC.DEF.GHI.").catch(console.error);
+  var dots = (res.match(/\./g) || []).length
+  console.log(`res=${res}`);
+  if (!res.startsWith("ABC.DEF.GHI..") || res.length != 39 || dots != 6) {
+    console.error(`Error: string returned by tmp_name expected to start with 'ABC.DEF.GHI..', got ${res}`);
+    process.exitCode = -1; return;
+  }
+
+  console.log("Test: _tmp_name -d...");
+  res = await zoau.datasets._tmp_name(null, {"debug":true}).catch(console.error);
+  console.log('exit=<' + res["exit"] + '>');
+  console.log('stdout=<' + res["stdout"] + '>');
+  console.log('stderr=<' + res["stderr"] + '>');
+  if (!res["stdout"].trimEnd("\n").startsWith("MVSTMP")) {
+    console.error(`Error: string returned by '_tmp_name -d' stdout expected to start with 'MVSTMP.', got ${res["stdout"].trimEnd("\n")}`);
+    process.exitCode = -1; return;
+  }
 
   console.log("All tests passed.");
  } catch(err) {
